c58c675eb2229e4ff81a4dbd25a5de9ad7e80741
mkapry <mkapry@yandex.ru>
2018-11-04 17:35:31 +0300

    hw2and3

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        loader: 'babel-loader',
-        include: __dirname + '/src/app/create',
-       },
-       {
-        test: /shadow\.css$/,
-        loader: 'css-loader',
-        include: __dirname + '/src',
-       }
-    ],
-  }
+      module: {
+        rules: [
+          {
+            test: /\.js/,
+            loader: 'babel-loader',
+            include: __dirname + '/src/app/create',
+          },
+          {
+            test: /shadow\.css$/,
+            loader: 'css-loader',
+            include: __dirname + '/src',
+          },
+        ]
+      }
 };
